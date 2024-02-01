5776288c56b6ca67228e9b0143b6a9f62da55853
Sonny Vesali <sonny.vesali@protonmail.com>
2024-01-31 19:46:54 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { animate } from 'motion';
+
+	onMount(() => {
+		animate(
+			'.box',
+			{ position: 'relative', top: '40vh', left: '20vw' },
+			{
+				duration: 5,
+				easing: 'ease-in-out',
+				direction: 'alternate'
+			}
+		);
+	});
+</script>
+
+<div>
+	<div class="box"></div>
+</div>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		padding: 0;
+	}
+	div {
+		height: 100vh;
+		width: 100vw;
+		background-color: black;
+	}
+	.box {
+		position: relative;
+		top: 0vh;
+		left: 20vw;
+		background-color: red;
+		width: 10vh;
+		height: 10vh;
+	}
+</style>
