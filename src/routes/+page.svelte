db32331351e6967d14979a34d9a9ce331842a90d
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-02-25 17:46:53 -0600

    add: primitive TearOfAllah impl

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { animate } from 'motion';
+	import { tearDrop, circle } from '../components/TearOfAllah/constants';
+	import { linear as easing } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	import { interpolateString } from 'd3-interpolate';
 
+	let selectedPath = tearDrop;
+	function select(path: string) {
+		selectedPath = path;
+	}
+	let animated_path = tweened(tearDrop, {
+		interpolate: interpolateString,
+		delay: 1000,
+		duration: 2000,
+		easing
+	});
+	$: animated_path.set(selectedPath);
 	onMount(() => {
 		animate(
 			'.box',
@@ -12,17 +27,29 @@
 				direction: 'alternate'
 			}
 		);
+		select(circle);
 	});
 </script>
 
 <div>
-	<div class="box"></div>
+	<div class="box">
+		<svg>
+			<path d={$animated_path} fill="none" stroke="white" />
+		</svg>
+	</div>
 </div>
 
 <style>
 	:global(html, body) {
 		margin: 0;
 		padding: 0;
+	}
+	svg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
 	}
 	div {
 		height: 100vh;
@@ -31,9 +58,9 @@
 	}
 	.box {
 		position: relative;
-		top: 0vh;
+		top: 10vh;
 		left: 20vw;
-		background-color: red;
+		background-color: rgba(255, 255, 255, 0.01);
 		width: 10vh;
 		height: 10vh;
 	}
