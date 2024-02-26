eb4216d78dc7132b831178daeb26bc60f550099a
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-02-25 18:08:26 -0600

    refactor: TearOfAllah logic into own component

---
+++ src/components/TearOfAllah/TearOfAllah.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { tearDrop, circle } from './constants';
+	import { linear as easing } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+	import { interpolateString } from 'd3-interpolate';
+
+	let selectedPath = tearDrop;
+	function select(path: string) {
+		selectedPath = path;
+	}
+	let animated_path = tweened(tearDrop, {
+		interpolate: interpolateString,
+		delay: 1000,
+		duration: 2000,
+		easing
+	});
+	$: animated_path.set(selectedPath);
+	select(circle);
+</script>
+
+<div class="box">
+	<svg>
+		<path d={$animated_path} fill="none" stroke="white" />
+	</svg>
+</div>
+
+<style>
+	svg {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+	}
+	.box {
+		position: relative;
+		top: 10vh;
+		left: 20vw;
+		background-color: rgba(255, 255, 255, 0.01);
+		width: 10vh;
+		height: 10vh;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { animate } from 'motion';
-	import { tearDrop, circle } from '../components/TearOfAllah/constants';
-	import { linear as easing } from 'svelte/easing';
-	import { tweened } from 'svelte/motion';
-	import { interpolateString } from 'd3-interpolate';
-
-	let selectedPath = tearDrop;
-	function select(path: string) {
-		selectedPath = path;
-	}
-	let animated_path = tweened(tearDrop, {
-		interpolate: interpolateString,
-		delay: 1000,
-		duration: 2000,
-		easing
-	});
-	$: animated_path.set(selectedPath);
+	import TearOfAllah from '../components/TearOfAllah/TearOfAllah.svelte';
 	onMount(() => {
 		animate(
 			'.box',
@@ -27,16 +12,11 @@
 				direction: 'alternate'
 			}
 		);
-		select(circle);
 	});
 </script>
 
 <div>
-	<div class="box">
-		<svg>
-			<path d={$animated_path} fill="none" stroke="white" />
-		</svg>
-	</div>
+	<TearOfAllah />
 </div>
 
 <style>
@@ -44,24 +24,9 @@
 		margin: 0;
 		padding: 0;
 	}
-	svg {
-		position: absolute;
-		top: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-	}
 	div {
 		height: 100vh;
 		width: 100vw;
 		background-color: black;
-	}
-	.box {
-		position: relative;
-		top: 10vh;
-		left: 20vw;
-		background-color: rgba(255, 255, 255, 0.01);
-		width: 10vh;
-		height: 10vh;
 	}
 </style>
